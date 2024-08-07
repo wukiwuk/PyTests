450116fdb682dd5aad30e493b0290353f690285a
wukiwuk <shaquille.fazhira@gmail.com>
2024-08-07 13:45:38 +0700

    Week 3 #3

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-try:
-    n = int(input("Enter a number: "))
-except ValueError:
-    print("Please enter a number")
-    n = int(input("Enter a number: "))
-else:
-    print(f"n")
+def main():
+    n = get_number()
+    print(f"Your number is {n}")
+
+def get_number():
+    while True:
+        try:
+            n = int(input("Enter a number: "))
+        except ValueError:
+            print("Please enter a number")
+        else:
+            return n
+
+
+main()
